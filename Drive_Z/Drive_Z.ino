5ae15b02952ea2d3b7b24d5a1edfb20c1c2a7240
ertex <davidlgjohansson@gmail.com>
2017-09-21 13:44:15 +0200

    It works and it's good enough

--- Drive_Z/Drive_Z.ino
+++ Drive_Z/Drive_Z.ino
@@ -1,19 +1,20 @@
-/* Code for driving the BO bot in a Z formation
+/* Code for driving the BOE bot in a Z formation
  *  By Emilie Karon Klefbom  and David Johansson
  *  September 14 2017
  *  v 1.0
  */
 
  #include <Servo.h>
-  int musicPin = 3;
+  int musicPin = 4;
  Servo servoL;
  Servo servoR;
  
 void setup() {
-  tone(4, 500, 300);
+  tone(musicPin, 500, 300);
   delay(1000);
   pinMode(musicPin,OUTPUT);
   servoAttach();
+  
 //20cm forward
 straightAcceleration(1);
 turnServos(100);
@@ -21,56 +22,58 @@ delay(100);
 
 //120deg right
 turnServos(10,100);
-delay(1300);
-pivitAcceleration(-1,135, false,true);
+delay(1250);
+pivitAcceleration(-1,125, false,true);
 
 //50cm forward
 straightAcceleration(1);
 turnServos(100);
-delay(1100);
+delay(1350);
 
 
 //120 left
 turnServos(100,10);
-delay(1450);
-pivitAcceleration(1,135, false,true);
+delay(1400);
+pivitAcceleration(1,130, false,true);
 
 
 
 //20cm forward
 straightAcceleration(1);
 turnServos(100);
-delay(100);
+delay(130);
+
 
 //180deg turn
-turnDR(180);
+turnDR(175);
+
 
 servoDisable();
-//playSong(musicPin);
+playSong(musicPin); //THIS IS WHEN IT TURNS
 delay(1000);
 servoAttach();
 
 //20cm forward
 straightAcceleration(1);
 turnServos(100);
-delay(100);
+delay(130);
 
 
 //120deg right
 turnServos(10,100);
-delay(1300);
-pivitAcceleration(-1,125, false,true);
+delay(1270);
+pivitAcceleration(-1,120, false,true);
 
 //50cm forward
 straightAcceleration(1);
 turnServos(100);
-delay(1050);
+delay(1350);
 
 
 //120 left
 turnServos(100,10);
-delay(1450);
-pivitAcceleration(1,125, false,true);
+delay(1400);
+pivitAcceleration(1,115, false,true);
 
 
 
