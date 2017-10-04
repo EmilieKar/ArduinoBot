8ddfbd85592bb2ca1b224528774d8bc790b83200
ertex <davidlgjohansson@gmail.com>
2017-10-04 12:29:00 +0200

    added IR labyrinth

---
+++ IR_labyrint/IR_labyrint.ino
@@ -0,0 +1,73 @@
+  #include <Servo.h>
+ Servo servoL;
+ Servo servoR;
+ double lastTime; 
+ boolean lightsOff=true;
+ int irLeft, irRight;
+ 
+void setup()                                 // Built-in initialization block
+{
+
+  tone(4, 3000, 1000);                       // Play tone for 1 second   
+  delay(1000);                               // Delay to finish tone   
+  pinMode(12, INPUT);  pinMode(9, OUTPUT);   // Left IR LED & Receiver
+  pinMode(3, INPUT); pinMode(2, OUTPUT);      //Right IR
+  Serial.begin(9600); 
+  pinMode(7, OUTPUT);
+ pinMode(6, OUTPUT);
+ digitalWrite(6,LOW);
+ digital
+  servoAttach();
+}
+  
+ 
+void loop()                                  // Main loop auto-repeats
+{
+  if(lightsOff){
+    
+  if(millis()>(lastTime+50)){                //makes a delay of 5ms 
+    lastTime = millis();
+    irLeft = irDetect(9, 12, 38000);       // Check for object                    // Display 1/0 no detect/detect
+    irRight = irDetect(2, 3, 38000);       // Check for object
+    Serial.print(irLeft);  
+    Serial.println(irRight);
+    
+  }
+
+  if(irLeft==1 && irRight==1){
+    digitalWrite(7,HIGH);
+    digitalWrite(6,HIGH);
+    forward();
+  }else if(irLeft==1 && irRight==0){
+      maneuver(90,-10);
+      digitalWrite(7,LOW);
+      digitalWrite(6,HIGH);
+  }else if(irLeft==0 && irRight==1){
+      maneuver(-10,90);
+      digitalWrite(7,HIGH);
+      digitalWrite(6,LOW);
+  }
+  else{
+    backwards();
+    digitalWrite(7,HIGH);
+    digitalWrite(6,LOW);
+    delay(100);
+    
+    }
+    
+
+                                 
+}
+}
+
+
+// IR Object Detection Function
+
+int irDetect(int irLedPin, int irReceiverPin, long frequency)
+{
+  tone(irLedPin, frequency, 8);              // IRLED 38 kHz for at least 1 ms   
+  delay(1);                                  // Wait 1 ms
+  int ir = digitalRead(irReceiverPin);       // IR receiver -> ir variable   
+  delay(1);                                  // Down time before recheck   
+  return ir;                                 // Return 1 no detect, 0 detect
+} 
---
+++ IR_labyrint/movefun.ino
@@ -0,0 +1,80 @@
+void servoAttach(){
+  servoL.attach(11);
+  servoR.attach(10);
+}  
+  
+void backwards() // Backward function
+{
+ 
+servoL.writeMicroseconds(1600); // Left wheel counterclockwise
+servoR.writeMicroseconds(1400); // Right wheel clockwise
+
+}
+
+void forward() // Backward function
+{
+
+servoL.writeMicroseconds(1450); // Left wheel clockwise
+servoR.writeMicroseconds(1550); // Right wheel counterclockwise
+
+}
+
+void turnTL(int time) // Left turn function
+{
+servoL.writeMicroseconds(1400); // Left wheel clockwise
+servoR.writeMicroseconds(1400); // Right wheel clockwise
+delay(time); // Maneuver for time ms
+servoStop();
+}
+
+void turnDL(int deg) // Right turn degrees
+{
+  int t = (deg / 0.176);
+  t-=25;
+  turnTL(t);
+}
+
+void turnTR(int time) // Right turn function
+{
+servoL.writeMicroseconds(1600); // Left wheel counterclockwise
+servoR.writeMicroseconds(1600); // Right wheel counterclockwise
+delay(time); // Maneuver for time ms
+servoStop();
+}
+
+void turnDR(int deg) // Right turn degrees
+{
+  int t = (deg / 0.176);
+  t -=25;
+  turnTR(t);
+}
+
+void turnDeg(int deg) // Right turn degrees
+{
+  if(deg >0 ){
+  turnDR(deg);
+  }
+  else{
+  turnDL(-deg);
+    }
+}
+
+
+
+void maneuver (int speedL, int speedR){
+  servoL.writeMicroseconds(1500 - speedL);
+  servoR.writeMicroseconds(1500 + speedR);  
+
+}
+
+void servoDisable() // Halt servo signals
+{
+servoL.detach(); // Stop sending servo signals
+servoR.detach();
+}
+
+void servoStop(){
+  servoL.writeMicroseconds(1500);
+  servoR.writeMicroseconds(1500);
+}
+
